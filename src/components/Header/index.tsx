cf39e37dd6b54ad6a03de778e6bcab3dfa3a7467
Guilherme de Prais <guilherme.prais@gmail.com>
2021-12-23 16:33:33 -0300

    feat: SignInButton

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 // import Image from 'next/image';
 
+import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
 
 export function Hedaer() {
@@ -13,6 +14,8 @@ export function Hedaer() {
           </a>
           <a href="#posts">Posts</a>
         </nav>
+
+        <SignInButton />
       </div>
     </header>
   );
