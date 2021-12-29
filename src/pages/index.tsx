b558fb951e3a77aa158d1efa3113aa47d59a22cc
Guilherme de Prais <guilherme.prais@gmail.com>
2021-12-29 12:21:24 -0300

    fix: change SSR to StaticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 
 import { SubscribeButton } from '../components/SubscribeButton';
@@ -41,7 +41,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1KC37aIwIiFjSRR19kEpAamT');
 
   const product = {
@@ -56,5 +56,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, // 24 hours,
   };
 };
